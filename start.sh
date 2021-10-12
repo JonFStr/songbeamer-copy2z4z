743e50e235074c64f9a254a1c785c7a2af9540d8
Alexander Dennenmoser <alexander.dennenmoser@outlook.de>
2021-10-12 22:20:35 +0200

    added privileged and adjusted docker-compose version to work on my system

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cd $(dirname $0)
 docker build -t songscopy .
-exec docker run --cap-add SYS_ADMIN --device /dev/fuse songscopy $@
+exec docker run --cap-add SYS_ADMIN --device /dev/fuse --privileged=True songscopy $@
