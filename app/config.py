3a9440b68413178d5b65a92be1d92105c47c45a9
Jonathan Straub <j.f.straub@gmx.de>
2024-07-04 17:11:13 +0200

    Add default config to image, but have possibility of override in docker-compose

---
+++ app/config.py
@@ -0,0 +1,7 @@
+delay = 10
+
+# The path to the rclone remote directory to synchronize
+remote = "remote:Songbeamer/Songs/"
+# Path to individual folders (inside the synchronized directory). Should end, but not start with '/'
+dir2 = "2-Zeilig/"
+dir4 = "4-Zeilig/"
